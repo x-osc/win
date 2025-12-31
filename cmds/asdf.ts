c3a8ef51039b2a0cf48d849a41ca5ec720a839e1
xosc <maxymcculloch@gmail.com>
2026-01-01 12:57:53 +1300

    oop

--- cmds/asdf.ts
+++ cmds/asdf.ts
@@ -3,7 +3,7 @@ import { CmdApi, CmdManifest } from "../core/cmd/command";
 import { sleep } from "../core/utils";
 
 async function launch(appApi: AppApi, cmdApi: CmdApi) {
-  let args = this.cmdApi.getArgs();
+  let args = cmdApi.getArgs();
   await sleep(500);
   cmdApi.writeLine("[" + args.join(", ") + "]");
   await sleep(200);
