30fc992984b3edb88167a5fb8b50e425846201fa
xosc <maxymcculloch@gmail.com>
2026-01-01 18:31:12 +1300

    forgot testapp :(

--- core/testApp.ts
+++ core/testApp.ts
@@ -1,49 +1,42 @@
 import type { AppApi } from "./app/api";
-import type { AppManifest, Process } from "./app/app";
+import type { AppManifest } from "./app/app";
 import { winDataBuilder } from "./wm/wm.svelte";
 
-class TestApp implements Process {
-  api: AppApi;
-
-  constructor(api: AppApi) {
-    this.api = api;
-  }
-
-  async launch() {
-    console.log("yipe it worke");
-    this.api.window
-      .createWindowAsync(winDataBuilder().withTitle("test_app").build())
-      .then((winApi) => {
-        console.log(winApi.getData().owner);
-        let body = winApi.getBody();
-        console.log(body);
-        body.innerHTML = `
-          <h1>welcom to my ebic app</h1>
-          <p>its got woords</p>
-          <p>so amaizing ama i rite gang</p>
-          <p>
-          askdjfkgljakdjdfsjflksdfmklsdnalvkjsdlfj
-          asdkjklsdmclvkmsdklfj
-          asdlkmclkamsdlkfjselfkml;kf;ldjflskadmf
-          jlksfjlisejrelsfjlsekmfldsfms;dfskd;jflkds
-          asjlkdfjvkladsfjtepotigpodjfklsjd
-          jskldfjsdlmfsd;fjoejfolsemklafjsldkjflkasdmnf
-          ashgsdkjelwfojeslfilsefjadlksfj;sdkfsaklfjdf
-          </p>
-        `;
-        winApi.on("focus", () => {
-          console.log("fjdkjfk");
-        });
-        winApi.on("close", () => {
-          console.log("asdfjkl");
-          alert("r u sure");
-          this.api.quit();
-        });
+async function launch(api: AppApi) {
+  console.log("yipe it worke");
+  api.window
+    .createWindowAsync(winDataBuilder().withTitle("test_app").build())
+    .then((winApi) => {
+      console.log(winApi.getData().owner);
+      let body = winApi.getBody();
+      console.log(body);
+      body.innerHTML = `
+        <h1>welcom to my ebic app</h1>
+        <p>its got woords</p>
+        <p>so amaizing ama i rite gang</p>
+        <p>
+        askdjfkgljakdjdfsjflksdfmklsdnalvkjsdlfj
+        asdkjklsdmclvkmsdklfj
+        asdlkmclkamsdlkfjselfkml;kf;ldjflskadmf
+        jlksfjlisejrelsfjlsekmfldsfms;dfskd;jflkds
+        asjlkdfjvkladsfjtepotigpodjfklsjd
+        jskldfjsdlmfsd;fjoejfolsemklafjsldkjflkasdmnf
+        ashgsdkjelwfojeslfilsefjadlksfj;sdkfsaklfjdf
+        </p>
+      `;
+      winApi.on("focus", () => {
+        console.log("fjdkjfk");
       });
-  }
+      winApi.on("close", () => {
+        console.log("asdfjkl");
+        alert("r u sure");
+        api.quit();
+      });
+    });
 }
 
 export let testAppManifest: AppManifest = {
   appId: "test_app",
-  createApp: (api: AppApi) => new TestApp(api),
+
+  launch,
 };
