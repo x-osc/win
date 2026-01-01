beae44c5135b630d351628341a2bd200084c3fab
xosc <maxymcculloch@gmail.com>
2026-01-01 19:27:13 +1300

    setup pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import path from "path";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/win/" : "/",
   plugins: [svelte()],
   resolve: {
     alias: {
       $lib: path.resolve(__dirname, "./core"),
     },
   },
-});
+}));
