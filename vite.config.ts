c03ab9538e9b64c30bbccc655d4928d35cd2efe4
xosc <urlocaldumbass@proton.me>
2026-01-06 17:33:44 +1300

    preload font

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import path from "path";
+import UnpluginInjectPreload from "unplugin-inject-preload/vite";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
   base: command === "build" ? "/win/" : "/",
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    UnpluginInjectPreload({
+      files: [
+        {
+          entryMatch: /Cozette.*\.woff$/,
+        },
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       "@core": path.resolve(__dirname, "./core"),
