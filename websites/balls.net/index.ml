048436d1854aaf2f60a68f6846a3eb1c22199a99
xosc <urlocaldumbass@proton.me>
2026-01-10 23:46:00 +1300

    balls

--- websites/balls.net/index.ml
+++ websites/balls.net/index.ml
@@ -2,5 +2,10 @@
 <text>
   welcome to balls dot net !!!!! we love balls !! we have balls of all different shapes and sizes !!
   we have small bouncy rubber balls, large beach balls and anything in between !!!!
-  we think all ball sizes should be valued and treasured !!
+  we think all ball sizes should be valued and treasured !! we have one ball and two balls !!
+  red balls and blue balls !! black and blue balls !! old balls and new balls !!
+  what a lot of balls there are !!!1! theres sad and glad balls !! very very bad balls !!
+  why are some balls sad glad and bad ????!!?!?!?!? go ask ur dad lmao gottem !!
+  some balls are thin and some are fat !! this one even has a yellow hat!!!
+  isnt the world a marvellous place ?!?!?!?!?!?
  </text>
