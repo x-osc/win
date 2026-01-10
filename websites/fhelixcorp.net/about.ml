b1b238a005a73c5fc44babf20b59ddda75b0b9aa
xosc <urlocaldumbass@proton.me>
2026-01-11 02:51:28 +1300

    about

---
+++ websites/fhelixcorp.net/about.ml
@@ -0,0 +1,3 @@
+<text>well, you may be wondering what we do</text>
+<text>um you see, we steal food from small homeless victorian children</text>
+<text>and then we play 100 gecs to them so they spontaneously combust</text>
