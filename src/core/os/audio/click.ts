1f9837fa027c825f1a7f3794fa07f3310aa08d42
xosc <urlocaldumbass@proton.me>
2026-01-11 23:16:37 +1300

    we using howler js actually

--- src/core/os/audio/click.ts
+++ src/core/os/audio/click.ts
@@ -1,26 +1,12 @@
 import clickSoundFile from "@assets/mouse03.wav?url";
+import { Howl } from "howler";
 
-const audioCtx = new window.AudioContext();
-let buffer: AudioBuffer | null = null;
-
-async function loadSounds() {
-  const response = await fetch(clickSoundFile);
-  const arrayBuffer = await response.arrayBuffer();
-  buffer = await audioCtx.decodeAudioData(arrayBuffer);
-}
-
-loadSounds();
+const clickSound = new Howl({
+  src: [clickSoundFile],
+  volume: 0.3,
+  preload: true,
+});
 
 export async function playClick() {
-  if (audioCtx.state === "suspended") {
-    await audioCtx.resume();
-    return;
-  }
-
-  if (buffer) {
-    const source = audioCtx.createBufferSource();
-    source.buffer = buffer;
-    source.connect(audioCtx.destination);
-    source.start(0);
-  }
+  clickSound.play();
 }
