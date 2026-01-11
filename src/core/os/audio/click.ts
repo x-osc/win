b660b237da9c482c61fca35a495f2bb95e03c6bb
xosc <urlocaldumbass@proton.me>
2026-01-11 23:04:54 +1300

    use web audio api

--- src/core/os/audio/click.ts
+++ src/core/os/audio/click.ts
@@ -1,8 +1,26 @@
-import clickSoundFile from "@assets/mouse03.wav";
+import clickSoundFile from "@assets/mouse03.wav?url";
 
-const clickSound = new Audio(clickSoundFile);
-clickSound.volume = 0.3;
+const audioCtx = new window.AudioContext();
+let buffer: AudioBuffer | null = null;
 
-export function playClick() {
-  clickSound.play();
+async function loadSounds() {
+  const response = await fetch(clickSoundFile);
+  const arrayBuffer = await response.arrayBuffer();
+  buffer = await audioCtx.decodeAudioData(arrayBuffer);
+}
+
+loadSounds();
+
+export async function playClick() {
+  if (audioCtx.state === "suspended") {
+    await audioCtx.resume();
+    return;
+  }
+
+  if (buffer) {
+    const source = audioCtx.createBufferSource();
+    source.buffer = buffer;
+    source.connect(audioCtx.destination);
+    source.start(0);
+  }
 }
