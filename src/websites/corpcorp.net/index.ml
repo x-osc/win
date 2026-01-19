bc3f48d518423a1a2a1d767cc6bcb6740ab91a48
Felix <felixmcwong+Github@gmail.com>
2026-01-19 14:00:54 +1300

    more dark and mysterious sites

---
+++ src/websites/corpcorp.net/index.ml
@@ -0,0 +1,4 @@
+<heading>Corp Corporation</heading>
+<text>We are an non-evil organization</text>
+<text>YUHHUHHUHHHHH >:) (totally ;P)</text>
+<text>do not ask us about money laundering in any cases</text>
--- src/websites/darkandmysterious.net/index.ml
+++ src/websites/darkandmysterious.net/index.ml
@@ -1,3 +1,4 @@
 <heading>You should not see this</heading>
 <text>We are an evil organization</text>
 <text>YUHHUHHUHHHHH >:)</text>
+<text>we do lots of money laundering through <link to="corpcorp.net">Corp Corp</link></text>
